3c9fcd74ed20eb34cab3b60e3f9879a4e0bce3f5
Balaphani <phnkumar51@gmail.com>
2025-06-21 16:36:07 +0530

    asc

--- module-infra/main.tf
+++ module-infra/main.tf
@@ -18,7 +18,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
 
 resource "aws_vpc_security_group_ingress_rule" "app_port" {
   security_group_id = aws_security_group.tool.id
-  cidr_ipv6         = "0.0.0.0/0"
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = var.port
   ip_protocol       = "tcp"
   to_port           = var.port
@@ -27,10 +27,9 @@ resource "aws_vpc_security_group_ingress_rule" "app_port" {
 
 resource "aws_vpc_security_group_egress_rule" "egress_allow_all" {
   security_group_id = aws_security_group.tool.id
-  cidr_ipv6         = "0.0.0.0/0"
+  cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
-
 
 resource "aws_instance" "tool" {
   ami                    = var.ami_id
